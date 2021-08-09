92a03de7e88b9b966c2f9728d87034dd712ed2f3
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-09 17:05:20 +0100

    Pass data

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html',
+         extra_context={'title': 'My Title'})),
 ]
