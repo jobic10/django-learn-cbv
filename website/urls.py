6dfdf6b84bf439e51d307b3e3ef03357422bac34
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-08-09 17:28:34 +0100

    ✨ New feature

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import Page2
+
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html',
          extra_context={'title': 'My Title'})),
+    path('second', Page2.as_view()),
+
 ]
